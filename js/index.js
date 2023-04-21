d91f85f3576de4d443e4cc2ecd44e8c2b8288a95
Aleksandr <newsubj9@yandex.ru>
2023-04-21 20:19:27 +0800

    Все

--- js/index.js
+++ js/index.js
@@ -9,3 +9,26 @@ servicesButtons.forEach((button, index) => {
     optionsContent[index].classList.add('options-item-shown');
   });
 });
+
+
+/** slider-buttons */
+
+const sliderTabButtons = document.querySelectorAll('.slider-button');
+const slides = document.querySelectorAll('.slider-item');
+const buttonPrevious = document.querySelector('.slider-arrow-button-left');
+const buttonNext = document.querySelector('.slider-arrow-button-right');
+let counter = 0;
+
+const clearClasses = () => {
+  document.querySelector('.slider-item-shown').classList.remove('slider-item-shown');
+  document.querySelector('.slider-button-current').classList.remove('slider-button-current');
+};
+
+sliderTabButtons.forEach((tab, index) => {
+  tab.addEventListener('click', () => {
+    clearClasses();
+    counter = index;
+    slides[index].classList.add('slider-item-shown');
+    tab.classList.add('slider-button-current');
+  });
+});
